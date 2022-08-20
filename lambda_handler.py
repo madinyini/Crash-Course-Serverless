43baf4feb0fd0f6ed04240eb56f7b04cd933918c
Roberto Madinyá <roberto.madinya@ioet.com>
2022-08-20 12:49:39 -0500

    modify crashcourse

--- lambda_handler.py
+++ lambda_handler.py
@@ -2,14 +2,25 @@ import datetime
 
 
 def suma(event, context):
-    operando_a = event['operando_a']
-    operando_b = event['operando_b']
+    operando_a = event['num1']
+    operando_b = event['num2']
     if not isinstance(operando_a, int) or not isinstance(operando_b, int):
         raise Exception('Parametros no son numeros enteros')
     return {
         "status": 200,
         "message": "ok",
         "suma": operando_b + operando_a
+    }
+
+def resta(event, context):
+    operando_a = event['num1']
+    operando_b = event['num2']
+    if not isinstance(operando_a, int) or not isinstance(operando_b, int):
+        raise Exception('Parametros no son numeros enteros')
+    return {
+        "status": 200,
+        "message": "ok",
+        "suma": operando_b - operando_a
     }
 
 
